241c0a68342ffca436350d6dd06d4cfdd763e160
Ari <makegeek@icloud.com>
2021-09-29 13:44:35 -0400

    commiting penis

--- Scripts/Attack_animations.js
+++ Scripts/Attack_animations.js
@@ -1,1 +1,7 @@
 //attackRocket = new PIXI.Texture.from('');
+function attackAnimation(defender) {
+	if (defender.isAttacking == false) {
+		defender.texture = defender.textures.attack;
+		setTimeout(() => {defender.texture = defender.textures.idle;}, 100)
+	}	
+}
